3911d3d7eb5d9e7e3c1754a640e0305a9c81c0ab
Nelson Villalon <nelsonvillalon1234@gmail.com>
2026-07-09 23:47:10 -0400

    Preparar frontend para Vercel

--- frontend/src/core/PeerManager.ts
+++ frontend/src/core/PeerManager.ts
@@ -17,15 +17,20 @@ class PeerManager {
         this.peer = new RTCPeerConnection({
 
             iceServers: [
-
-                {
-                    urls: [
-                        "stun:stun.l.google.com:19302",
-                        "stun:stun1.l.google.com:19302"
-                    ]
-                }
-
-            ]
+  {
+    urls: [
+      "stun:openrelay.metered.ca:80"
+    ]
+  },
+  {
+    urls: [
+      "turn:openrelay.metered.ca:80",
+      "turn:openrelay.metered.ca:443"
+    ],
+    username: "openrelayproject",
+    credential: "openrelayproject"
+  }
+]
 
         });
         this.peer.onsignalingstatechange = () => {
